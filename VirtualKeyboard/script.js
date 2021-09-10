22e16ae11939e199debce4229b1530e260b54de5
shmailez <60861135+shmailez@users.noreply.github.com>
2021-09-10 18:33:57 +0600

    Add files via upload
    with notworking commentarea

--- VirtualKeyboard/script.js
+++ VirtualKeyboard/script.js
@@ -5,9 +5,27 @@ let buttonClear = document.querySelector('.clear');
 for (let buttonSymbol of buttonSymbols) {
   buttonSymbol.onclick = function() {
   display.innerHTML += buttonSymbol.innerHTML;
+
 }
 }
 
 buttonClear.onclick = function () {
   display.textContent = ' ';
+}
+
+var i = 0;
+function newmes(){
+
+    
+    var textmes = document.getElementById('textmes');
+    
+    
+    var textmessage = textmes.value;
+    
+   
+    var comments = document.getElementById('comments');
+    
+    i++;
+    comments.innerHTML += '<div class="com" class="comment"> >  <p> ' + textmessage + ' </p></div></div>';
+    
 }
