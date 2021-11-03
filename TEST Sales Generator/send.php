a2d033770a6efa91ed619aafe1d9f20f0df8f769
shmailez <60861135+shmailez@users.noreply.github.com>
2021-11-03 18:03:51 +0600

    Add files via upload
    php code corrected

--- TEST Sales Generator/send.php
+++ TEST Sales Generator/send.php
@@ -1,9 +1,9 @@
-<?php
 
-if(isset($_POST['submit'])){
+
+<!-- if(isset($_POST['submit'])){
 $to = "shmailez@gmail.com";; // Здесь нужно написать e-mail, куда будут приходить письма
 $from = $_POST['email']; // this is the sender's Email address
-$first_name = $_POST['first_name'];
+$first_name = $_POST['tel'];
 $subject = "Форма отправки сообщений с сайта";
 $subject2 = "Copy of your form submission";
 $message = $first_name . " оставил сообщение:" . "\n\n" . $_POST['message'];
@@ -17,6 +17,31 @@ mail($to,$subject,$message,$headers);
 echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
 echo "<br /><br /><a href=''>Вернуться на сайт.</a>";
 
-}
+} -->
 
+
+<?php
+if($_POST['submit']) {
+/* Устанавливаем e-mail Кому и от Кого будут приходить письма */    
+    $to = "shmailez@gmail.com"; // Здесь нужно написать e-mail, куда будут приходить письма  
+    $from = "nfeaw@mail.ru"; // Здесь нужно написать e-mail, от кого будут приходить письма, например no-reply@epicblog.net
+
+/* Указываем переменные, в которые будет записываться информация с формы */
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $subject = "Форма отправки сообщений с сайта";//Фиксированная тема письма
+
+
+/* Переменная, которая будет отправлена на почту со значениями, вводимых в поля */
+$mail_to_myemail = "Здравствуйте! 
+Было отправлено сообщение с сайта! 
+Имя отправителя: $name 
+Номер телефона: $phone 
+Чтобы ответить на письмо, создайте новое сообщение, скопируйте электронный адрес и вставьте в поле Кому.";  
+
+$headers = "From: $from \r\n";
+
+/* Отправка сообщения, с помощью функции mail() */
+    mail($to, $subject, $mail_to_myemail, $headers . 'Content-type: text/plain; charset=utf-8');
+}
 ?>
