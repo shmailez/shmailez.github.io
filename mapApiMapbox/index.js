1d1c7cd36453f043715eda29b557d92c9a21a43f
shmailez <60861135+shmailez@users.noreply.github.com>
2021-12-10 20:59:08 +0600

    Add files via upload
    change geocoordinate

--- mapApiMapbox/index.js
+++ mapApiMapbox/index.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicGFycWluYSIsImEiOiJja3d6ZTlxM3IwaHJvMnBsNHNpb
 const map = new mapboxgl.Map({
 container: 'map', // container ID
 style: 'mapbox://styles/mapbox/streets-v11', // style URL
-center: [76.9673, 52.2505], // starting position [lng, lat]
+center: [76.9673, 52.2515], // starting position [lng, lat]
 zoom: 15 // starting zoom
 });
 
@@ -10,37 +10,37 @@ const marker1 = new mapboxgl.Marker() ///add geoMarcker
 .setLngLat([76.96995, 52.24911])
 .addTo(map);
 
-const marker2 = new mapboxgl.Marker() ///add geoMarcker
+const marker2 = new mapboxgl.Marker({color: 'red'}) ///add geoMarcker
 .setLngLat([76.9645, 52.2538])
 .addTo(map);
 
 
 map.addControl(new mapboxgl.FullscreenControl());
 
-
+///add way point-to-point
 map.on('load', () => {
-map.addSource('route', {
-'type': 'geojson',
-'data': {
-'type': 'Feature',
-'properties': {},
-'geometry': {
-'type': 'LineString',
-'coordinates': [
+    map.addSource('route', {
+        'type': 'geojson',
+        'data': {
+            'type': 'Feature',
+            'properties': {},
+            'geometry': {
+                'type': 'LineString',
+                'coordinates': [
 
-[76.96995, 52.24911],
-[76.9693, 52.24962],
-[76.9689, 52.24945],
-[76.9681, 52.25],
-[76.9673, 52.2505],
-[76.9662, 52.2513],
-[76.9659, 52.2535],
-[76.9645, 52.2535],
-[76.9645, 52.2538],
-]
-}
-}
-});
+                    [76.96995, 52.24911],
+                    [76.9693, 52.24962],
+                    [76.9689, 52.24945],
+                    [76.9681, 52.25],
+                    [76.9673, 52.2505],
+                    [76.9662, 52.2513],
+                    [76.9659, 52.2535],
+                    [76.9645, 52.2535],
+                    [76.9645, 52.2538],
+                ]
+            }
+        }
+    });
 map.addLayer({
 'id': 'route',
 'type': 'line',
